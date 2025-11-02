17ca25bea507e840510cf2d3d18eeeffcd88b7ec
Caitlyn Kim <cakim@smith.edu>
2025-11-02 13:50:41 -0500

    starting phase 1 implementation

---
+++ WordValidation.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+import a5.SpellingOperations;
+
+public class WordValidation implements SpellingOperations{
+    /**
+     *  @param query the word to check
+     *  @return true if the query word is in the dictionary.
+     */
+    public boolean containsWord(String query) {
+        return true;
+    }
+
+    /**
+     *  @param query the word to check
+     *  @return a list of all valid words that are one edit away from the query
+     */
+    public ArrayList<String> nearMisses(String query) {
+        return ArrayList<String>;
+    }
+
+    public HashSet<String>(String filename) {
+        HashSet<String> fileName = new HashSet<>(filename);
+        return fileName;
+    }
+}
